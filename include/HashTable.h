2c1a6e7de242252b0c74ec51e102ce7b4cfbfdc9
vilondz <alu0101573052@ull.edu.es>
2026-03-19 22:02:17 +0000

    se hizo lo que se puso

--- include/HashTable.h
+++ include/HashTable.h
@@ -1,11 +1,35 @@
+#include "DispersionFunction.h"
+#include "ExplorationFunction.h"
+#include "Sequence.h"
+#include "StaticSequence.h"
+#include <vector>
+#include <memory>
 
-template <typename key, typename container>
+template <typename key, class container = StaticSequence<key>>
 class HashTable {
   private:
-    std::vector<container> tabla_hash_;
-    int tablesize_;
+    unsigned blocksize_;
+    std::vector<container>table_;
+    unsigned tablesize_;
+    std::unique_ptr<DispersionFunction> fd_;
+    std::unique_ptr<ExplorationFunction> fe_;
   public:
-    HashTable(int);
-    ~HashTable(){}
-    
+    HashTable(unsigned, DispersionFunction<key>&, ExplorationFunction<key>&, unsigned);
+    ~HashTable(){} 
 };
+
+template <typename key, class container>
+HashTable<key,container>::HashTable(unsigned tablesize, DispersionFunction<key>& fd,
+                                    ExplorationFunction<key>& fe, unsigned blocksize){
+tablesize_ = tablesize;
+blocksize_ = blocksize;
+fd_ = fd;
+fe_ = fe;
+for(int i = 0; i< tablesize_; i++){
+  table_.push_back(StaticSequence(blocksize_));
+}
+}
+
+
+//clase especializada 
+
